a574a98a7076a1fa43008cbed1c4f06e29badb2f
Oleksandr Vlasov <drumlife182@gmail.com>
2026-09-14 09:57:35 +0200

    feat: add InboxScreen with Redux store and MSW mocking

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
-import React from "react";
+import './index.css';
+import store from './lib/store';
+
+import { Provider } from 'react-redux'
+import InboxScreen from './components/InboxScreen/InboxScreen';
 
 function App() {
-  return <div>Hello Vite</div>;
+  <Provider store={store}>
+    <InboxScreen />
+  </Provider>
 }
 
 export default App;
